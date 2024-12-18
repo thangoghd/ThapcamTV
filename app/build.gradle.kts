9a75069ed8bb5edbce6dc3643a6aba2b54ad8f56
thangoghd <thangteenx2@gmail.com>
2024-12-18 21:38:15 +0700

    Changer app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.thangoghd.thapcamtv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
     }
 
